1b298c138b3abfd0262a33690e31831de0e42428
alessandrofalcoo <alefalco.1003@gmail.com>
2025-08-20 11:38:12 +0200

    fix edit in GET in PizzaController and add "ingredients" attribute to the view

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/IngredientController.java
@@ -3,6 +3,7 @@ package org.lessons.java.spring_la_mia_pizzeria_crud.controller;
 import java.util.List;
 
 import org.lessons.java.spring_la_mia_pizzeria_crud.model.Ingredient;
+import org.lessons.java.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.lessons.java.spring_la_mia_pizzeria_crud.repository.IngredientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,20 +21,27 @@ import jakarta.validation.Valid;
 @RequestMapping("/ingredients")
 public class IngredientController {
     @Autowired
-    private IngredientRepository repository;
+    private IngredientRepository ingredientRepository;
 
     @GetMapping
     public String index(Model model) {
-        List<Ingredient> ingredients = repository.findAll();
+        List<Ingredient> ingredients = ingredientRepository.findAll();
         model.addAttribute("ingredients", ingredients);
         return "ingredients/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") Integer id, Model model) {
-        Ingredient ingredient = repository.findById(id).get();
+        Ingredient ingredient = ingredientRepository.findById(id).get();
         model.addAttribute("ingredient", ingredient);
         return "ingredients/show";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("ingredient", new Ingredient());
+
+        return "ingredients/create-or-edit";
     }
 
     @PostMapping("/create")
@@ -43,7 +51,39 @@ public class IngredientController {
             return "ingredients/create-or-edit";
         }
 
-        repository.save(formIngredient);
-        return "redirect:/ingredients/index";
+        ingredientRepository.save(formIngredient);
+        return "redirect:/ingredients";
     }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model) {
+        model.addAttribute("ingredients", ingredientRepository.findById(id).get());
+        model.addAttribute("edit", true);
+        return "ingredients/create-or-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String edit(@Valid @ModelAttribute("ingredient") Ingredient ingredient, BindingResult bindingResult,
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("edit", true);
+            return "ingredients/create-or-edit";
+        }
+
+        ingredientRepository.save(ingredient);
+        return "redirect:/ingredients";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id) {
+        Ingredient ingredientToDelete = ingredientRepository.findById(id).get();
+        for (Pizza linkedPizza : ingredientToDelete.getPizze()) {
+            linkedPizza.getIngredients().remove(ingredientToDelete);
+        }
+
+        ingredientRepository.delete(ingredientToDelete);
+        return "redirect:/ingredients";
+    }
+
 }
